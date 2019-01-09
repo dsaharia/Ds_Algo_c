5b048a1354121e3590351459a3ab03dea912396c
Dhiraj Saharia <dsaharia@outlook.com>
2019-01-09 21:44:51 +0530

    Added Merge Sort

--- algo/merge_sort.c
+++ algo/merge_sort.c
@@ -2,15 +2,68 @@
 #include <stdlib.h>
 #include "my_swapping.c"
 
-void merge(int arr[], int l, int m, int r)
+void merge(int arr[], int l, int mid, int r)
 {
+    int i,j,k;
+    int nl = mid - l +1; // no of elements in left subarray.
+    int nr = r - mid;  // no of elements in right subarray.
+    int al[nl], ar[nr]; // declare the temp arrays.
+    for (i=0; i<nl; i++)
+    {
+	al[i] = arr[l+i];
+    }
 
+    for (j=0;j<nr;j++)
+    {
+	ar[j] = arr[mid+1+j];
+    }
+    i=j=0; // for tracking first and second subarray elements.
+    k=l; // tracking the emrged array.
 
+    while(i<nl && j<nr)
+    {
+	if (al[i] <= ar[j])
+	{
+	    arr[k] = al[i];
+	    i++;
+	}
+	else
+	{
+	    arr[k] = ar[j];
+	    j++;
+	}
+	k++;
+    
+    }
+    // If one sub array finishes first
+    while (i<nl)
+    {
+	arr[k] = al[i];
+	i++;
+	k++;
+    }
+    while (j<nr)
+    {
+	arr[k] = ar[j];
+	j++;
+	k++;
+    
+    }
 }
 
-void merge_sort(int arr[], int l, int r)
+void mergeSort(int arr[], int l, int r)
 {
+    int mid;
+
+    if (l < r)
+    {
+	mid = l + (r - l)/2;
+
+	mergeSort(arr, l, mid);
+	mergeSort(arr, mid+1, r);
+	merge(arr, l, mid, r);
     
+    }
 
 }
 
@@ -35,7 +88,7 @@ int main()
     {
 	scanf("%d", &arr[i]);
     }
-    merge_sort(arr, 0, size-1);
+    mergeSort(arr, 0, size-1);
     printf("--- Merge sort --- \nElements in sorted order\n");
     print(arr,size);
 
